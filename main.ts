6a60d5994fc369ebdda204e2cd36a471b9e7cbe1
ppilaszewicz <71039101+ppilaszewicz@users.noreply.github.com>
2022-02-22 10:41:08 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Duck)
+    music.playMelody("E E F F F D D C ", 120)
+})
+basic.showIcon(IconNames.Skull)
+music.playMelody("E F - G F D A C ", 120)
+basic.forever(function () {
+	
+})
